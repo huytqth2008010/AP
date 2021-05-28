2452e64cdbdcaaa236f20cea9d50485e531af131
Huy <huytqth2008010@fpt.edu.vn>
2021-05-28 10:48:27 -0700

    hehe

---
+++ AssignmentOne/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace T2008M_AP.AssignmentOne
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            TrendSetting trendSetting = new TrendSetting();
+            trendSetting.Sales();
+        }
+    }
+}
