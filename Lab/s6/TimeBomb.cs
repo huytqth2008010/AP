073abdd93aeff245171c8902cc05ab1b400e504f
Huy <huytqth2008010@fpt.edu.vn>
2021-05-28 08:50:52 -0700

    aa

---
+++ Lab/s6/TimeBomb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace T2008M_AP.Lab.s6
+{
+    public class TimeBomb
+    {
+        public static void Main(string[] args)
+        {
+            new Thread(delegate(Object obj)
+            {
+                for (int i = 9; i >= 0; i--)
+                {
+                    for (int j = 59; j >= 0; j--)
+                    {
+                        try
+                        {
+                            Console.WriteLine(i + " : " + j);
+                            Thread.Sleep(100);
+                            Console.Clear();
+                        }
+                        catch (Exception e)
+                        {
+                        }
+                    }
+                }
+                Console.WriteLine("BÙM ...");
+            }).Start();
+        }
+    }
+}
